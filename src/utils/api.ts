9ab13c1b6b24cc6636c67ad97ab687e783134ea9
sardormahmudovbellissimo <sardor.mahmudov@bellissimo.uz>
2025-09-22 15:45:11 +0500

    auth for playmobile added

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -3,3 +3,15 @@ import axios from "axios"
 export const playMobileApi = axios.create({
   baseURL: process.env.PLAY_MOBILE_BASE_URL
 })
+
+export const setBasicAuth = (username: string, password: string) => {
+  playMobileApi.defaults.auth = {username, password}
+}
+
+export const clearAuth = () => {
+  playMobileApi.defaults.auth = undefined
+}
+
+if (process.env.PLAY_MOBILE_USERNAME && process.env.PLAY_MOBILE_PASSWORD) {
+  setBasicAuth(process.env.PLAY_MOBILE_USERNAME, process.env.PLAY_MOBILE_PASSWORD)
+}
